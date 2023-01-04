355019f0916864428d87b0eadbf4e45f2a7c5577
paidra_to <uku.kert.paidra@ut.ee>
2023-01-04 14:52:10 +0200

    Added requirements.txt and removed ROS as a dependency

--- rosbag_splitter.py
+++ rosbag_splitter.py
@@ -1,32 +1,35 @@
-import subprocess
+#!/usr/bin/env python3
 import argparse
 import os
+import yaml
+import rosbag
+import rospy
+import logging
+from tqdm import tqdm
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 
 
-def main():
-    '''
-    This script is for easy cutting of ROSBAGS
-
-    Usage:
-    $ rosbag_splitter.py -i  INPUT_ROSBAG_PATH.bag -o (Optional) OUTPUT_ROSBAG_FILE_NAME.bag -b BEGIN_TIME_IN_S -e (Optional) END_TIME_IN_S
-
-    '''
+def parse_args():
     parser = argparse.ArgumentParser(
         description="This script is used for cutting a rosbag")
     parser._action_groups.pop()
     required = parser.add_argument_group('required arguments')
     optional = parser.add_argument_group('optional arguments')
 
-    required.add_argument('-i', '--input_bag', type=str,
+    required.add_argument('-i', '--input-bag', type=str,
                           help="Path to bag to cut", required=True)
-    required.add_argument('-e', '--end_time', type=float,
+    required.add_argument('-e', '--end-time', type=float,
                           help="End time in seconds from begginning of bag", required=True)
-    optional.add_argument('-o', '--output_bag', type=str, default="",
+    optional.add_argument('-o', '--output-bag', type=str, default="",
                           help="Specify output bag name. Path is the same as input path")
-    optional.add_argument('-b', '--begin_time', type=float, default=0.0,
+    optional.add_argument('-b', '--begin-time', type=float, default=0.0,
                           help="Starting time in seconds from beginning of bag, default is 0.0")
-    args = parser.parse_args()
+    return parser.parse_args()
 
+
+if __name__ == "__main__":
+    args = parse_args()
     input_bag_path = args.input_bag
 
     # Perform argument validity checks
@@ -38,11 +41,11 @@ def main():
 
     # Output bag path name creation and validation
     if args.output_bag == "":
-        output_bag = os.path.splitext(args.input_bag)[0]+"_cut.bag"
+        output_bag_path = os.path.splitext(args.input_bag)[0]+"_cut.bag"
     elif not args.output_bag.endswith(".bag"):
-        output_bag = output_dir + args.output_bag+".bag"
+        output_bag_path = output_dir + args.output_bag+".bag"
     else:
-        output_bag = output_dir + args.output_bag
+        output_bag_path = output_dir + args.output_bag
 
     begin_time = args.begin_time
     end_time = args.end_time
@@ -50,24 +53,14 @@ def main():
     if end_time <= begin_time:
         raise ValueError("End time cannot be smaller or equal to begin time")
 
-    # Run the  command "rosbag info" to get the UNIX timestamp of the start time
-    start_command = ['rosbag', 'info', '-y', '-k start', input_bag_path]
-    start_time_of_bag = float(
-        subprocess.check_output(start_command).decode('UTF-8'))
+    logger.info(f"Reading input bag {input_bag_path}")
+    with rosbag.Bag(input_bag_path, 'r') as input_bag:
+        rosbag_info = yaml.safe_load(input_bag._get_yaml_info())
+        start_time_of_bag = rosbag_info['start']
+        cut_begin = rospy.Time.from_sec(start_time_of_bag + begin_time)
+        cut_end = rospy.Time.from_sec(start_time_of_bag + end_time)
 
-    # Generate the time filter of the rosbag
-    cut_begin = start_time_of_bag+begin_time
-    cut_end = start_time_of_bag + end_time
-    time_filter = 't.to_sec() >= {0} and t.to_sec() <= {1}'.format(
-        cut_begin, cut_end)
-    print("Using Filter:", time_filter)
-    print("Saving the cut bag as", output_bag)
-
-    # Execute the filter on the rosbag
-    filter_command = ['rosbag', 'filter',
-                      input_bag_path, output_bag, time_filter]
-    subprocess.run(filter_command)
-
-
-if __name__ == "__main__":
-    main()
+        with rosbag.Bag(output_bag_path, 'w') as output_bag:
+            logger.info(f"Saving the cut bag as {output_bag_path}")
+            for topic, msg, t in tqdm(input_bag.read_messages(start_time=cut_begin, end_time=cut_end)):
+                output_bag.write(topic, msg, t)
