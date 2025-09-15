07e7292b44e0b3593528f8f8fee0c50d97132992
Uku Kert Paidra <ukukert@gmail.com>
2025-09-15 13:32:11 +0300

    Formatted with RUFF

--- rosbag_splitter.py
+++ rosbag_splitter.py
@@ -1,30 +1,35 @@
 #!/usr/bin/env python3
 import argparse
+import logging
 import os
-import yaml
+
 import rosbag
 import rospy
-import logging
+import yaml
 from tqdm import tqdm
+
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(
-        description="This script is used for cutting a rosbag")
+    parser = argparse.ArgumentParser(description="This script is used for cutting a rosbag")
     parser._action_groups.pop()
-    required = parser.add_argument_group('required arguments')
-    optional = parser.add_argument_group('optional arguments')
+    required = parser.add_argument_group("required arguments")
+    optional = parser.add_argument_group("optional arguments")
 
-    required.add_argument('-i', '--input-bag', type=str,
-                          help="Path to bag to cut", required=True)
-    required.add_argument('-e', '--end-time', type=float,
-                          help="End time in seconds from begginning of bag", required=True)
-    optional.add_argument('-o', '--output-bag', type=str, default="",
-                          help="Specify output bag name.")
-    optional.add_argument('-b', '--begin-time', type=float, default=0.0,
-                          help="Starting time in seconds from beginning of bag, defaults to 0.0")
+    required.add_argument("-i", "--input-bag", type=str, help="Path to bag to cut", required=True)
+    required.add_argument(
+        "-e", "--end-time", type=float, help="End time in seconds from begginning of bag", required=True
+    )
+    optional.add_argument("-o", "--output-bag", type=str, default="", help="Specify output bag name.")
+    optional.add_argument(
+        "-b",
+        "--begin-time",
+        type=float,
+        default=0.0,
+        help="Starting time in seconds from beginning of bag, defaults to 0.0",
+    )
     return parser.parse_args()
 
 
@@ -37,13 +42,13 @@ if __name__ == "__main__":
         raise FileNotFoundError("The input rosbag could not be found")
 
     # Get the full path to the input file, because we want to save our bag to that dir
-    output_dir = os.path.dirname(os.path.abspath(input_bag_path))+'/'
+    output_dir = os.path.dirname(os.path.abspath(input_bag_path)) + "/"
 
     # Output bag path name creation and validation
     if args.output_bag == "":
-        output_bag_path = os.path.splitext(args.input_bag)[0]+"_cut.bag"
+        output_bag_path = os.path.splitext(args.input_bag)[0] + "_cut.bag"
     elif not args.output_bag.endswith(".bag"):
-        output_bag_path = output_dir + args.output_bag+".bag"
+        output_bag_path = output_dir + args.output_bag + ".bag"
     else:
         output_bag_path = output_dir + args.output_bag
 
@@ -54,13 +59,13 @@ if __name__ == "__main__":
         raise ValueError("End time cannot be smaller or equal to begin time")
 
     logger.info(f"Reading input bag {input_bag_path}")
-    with rosbag.Bag(input_bag_path, 'r') as input_bag:
+    with rosbag.Bag(input_bag_path, "r") as input_bag:
         rosbag_info = yaml.safe_load(input_bag._get_yaml_info())
-        start_time_of_bag = rosbag_info['start']
+        start_time_of_bag = rosbag_info["start"]
         cut_begin = rospy.Time.from_sec(start_time_of_bag + begin_time)
         cut_end = rospy.Time.from_sec(start_time_of_bag + end_time)
 
-        with rosbag.Bag(output_bag_path, 'w') as output_bag:
+        with rosbag.Bag(output_bag_path, "w") as output_bag:
             logger.info(f"Saving the cut bag as {output_bag_path}")
             for topic, msg, t in tqdm(input_bag.read_messages(start_time=cut_begin, end_time=cut_end)):
                 output_bag.write(topic, msg, t)
